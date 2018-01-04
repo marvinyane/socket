26d056017f701b4fcf815a56abcf8fe9a79b41ba
yanjunzheng <yanjunzheng@pset.suntec.net>
2018-01-04 16:01:21 +0800

    change to multicast mode?

--- packet/client.c
+++ packet/client.c
@@ -49,6 +49,26 @@ int main(int argc, char** argv)  {
     if (ioctl(packet_socket, SIOCGIFHWADDR, &if_mac) < 0)
         perror("SIOCGIFHWADDR");
 
+#if 0
+    struct packet_mreq mreq;
+    mreq.mr_ifindex = ifindex;
+    mreq.mr_type = PACKET_MR_MULTICAST;
+    mreq.mr_alen = 6;
+    mreq.mr_address[0] = 0x03;
+    mreq.mr_address[1] = 0x00;
+    mreq.mr_address[2] = 0x00;
+    mreq.mr_address[3] = 0x01;
+    mreq.mr_address[4] = 0x04;
+    mreq.mr_address[5] = 0x00;
+
+    error = setsockopt(packet_socket, SOL_PACKET, PACKET_ADD_MEMBERSHIP, &mreq, sizeof(mreq));
+    if (error) {
+        perror("set socketopt failed:");
+    }
+
+    printf("add to membership success.\n");
+#endif
+
     char sendbuf[1024];
     struct ether_header *eh = (struct ether_header*)sendbuf;
     memset(sendbuf, 0, sizeof(sendbuf));
@@ -59,12 +79,18 @@ int main(int argc, char** argv)  {
     eh->ether_shost[3] = ((unsigned char *)&if_mac.ifr_hwaddr.sa_data)[3];
     eh->ether_shost[4] = ((unsigned char *)&if_mac.ifr_hwaddr.sa_data)[4];
     eh->ether_shost[5] = ((unsigned char *)&if_mac.ifr_hwaddr.sa_data)[5];
-    eh->ether_dhost[0] = MY_DEST_MAC0;
-    eh->ether_dhost[1] = MY_DEST_MAC1;
-    eh->ether_dhost[2] = MY_DEST_MAC2;
-    eh->ether_dhost[3] = MY_DEST_MAC3;
-    eh->ether_dhost[4] = MY_DEST_MAC4;
-    eh->ether_dhost[5] = MY_DEST_MAC5;
+    /*eh->ether_dhost[0] = MY_DEST_MAC0;*/
+    /*eh->ether_dhost[1] = MY_DEST_MAC1;*/
+    /*eh->ether_dhost[2] = MY_DEST_MAC2;*/
+    /*eh->ether_dhost[3] = MY_DEST_MAC3;*/
+    /*eh->ether_dhost[4] = MY_DEST_MAC4;*/
+    /*eh->ether_dhost[5] = MY_DEST_MAC5;*/
+    eh->ether_dhost[0] = 0x03;
+    eh->ether_dhost[1] = 0x00;
+    eh->ether_dhost[2] = 0x00;
+    eh->ether_dhost[3] = 0x01;
+    eh->ether_dhost[4] = 0x04;
+    eh->ether_dhost[5] = 0x00;
 
     eh->ether_type = htons(0x86DC);
     int tx_len = sizeof(struct ether_header);
@@ -79,12 +105,18 @@ int main(int argc, char** argv)  {
     memset(&ifsock_addr, 0, sizeof(ifsock_addr));
     ifsock_addr.sll_family = AF_PACKET;
     ifsock_addr.sll_ifindex = ifindex;
-    ifsock_addr.sll_addr[0] = MY_DEST_MAC0;
-    ifsock_addr.sll_addr[1] = MY_DEST_MAC1;
-    ifsock_addr.sll_addr[2] = MY_DEST_MAC2;
-    ifsock_addr.sll_addr[3] = MY_DEST_MAC3;
-    ifsock_addr.sll_addr[4] = MY_DEST_MAC4;
-    ifsock_addr.sll_addr[5] = MY_DEST_MAC5;
+    /*ifsock_addr.sll_addr[0] = MY_DEST_MAC0;*/
+    /*ifsock_addr.sll_addr[1] = MY_DEST_MAC1;*/
+    /*ifsock_addr.sll_addr[2] = MY_DEST_MAC2;*/
+    /*ifsock_addr.sll_addr[3] = MY_DEST_MAC3;*/
+    /*ifsock_addr.sll_addr[4] = MY_DEST_MAC4;*/
+    /*ifsock_addr.sll_addr[5] = MY_DEST_MAC5;*/
+    ifsock_addr.sll_addr[0] = 0x03;
+    ifsock_addr.sll_addr[1] = 0x00;
+    ifsock_addr.sll_addr[2] = 0x00;
+    ifsock_addr.sll_addr[3] = 0x01;
+    ifsock_addr.sll_addr[4] = 0x04;
+    ifsock_addr.sll_addr[5] = 0x00;
     ifsock_addr.sll_halen = ETH_ALEN;
 
     error = sendto(packet_socket, sendbuf, tx_len, 0, (struct sockaddr*)&ifsock_addr, sizeof(ifsock_addr));
--- packet/server.c
+++ packet/server.c
@@ -30,11 +30,30 @@ int main(int argc, char** argv)  {
     ifindex = device.ifr_ifindex;
     printf("device interface index is %d.\n", ifindex);
 
+
+    /*add membership*/
+    struct packet_mreq mreq;
+    mreq.mr_ifindex = ifindex;
+    mreq.mr_type = PACKET_MR_MULTICAST;
+    mreq.mr_alen = 6;
+    mreq.mr_address[0] = 0x03;
+    mreq.mr_address[1] = 0x00;
+    mreq.mr_address[2] = 0x00;
+    mreq.mr_address[3] = 0x01;
+    mreq.mr_address[4] = 0x04;
+    mreq.mr_address[5] = 0x00;
+
+    error = setsockopt(packet_socket, SOL_PACKET, PACKET_ADD_MEMBERSHIP, &mreq, sizeof(mreq));
+    if (error) {
+        perror("set socketopt failed:");
+    }
+
     struct sockaddr_ll ifsock_addr;
     memset(&ifsock_addr, 0, sizeof(ifsock_addr));
     ifsock_addr.sll_family = AF_PACKET;
     ifsock_addr.sll_ifindex = ifindex;
     ifsock_addr.sll_protocol = htons(0x86DC);
+    ifsock_addr.sll_pkttype = PACKET_HOST | PACKET_MULTICAST;
     error = bind(packet_socket, (struct sockaddr *)&ifsock_addr, sizeof(ifsock_addr));
     if (error < 0) {
         perror("bind socket error:");
